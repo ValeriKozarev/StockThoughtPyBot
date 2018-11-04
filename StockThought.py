66eae03b6d9ce5b848d2ec2a70f43ecaf384d75c
vikozare <vikozare@ncsu.edu>
2018-11-03 21:32:59 -0400

    installed tweepy

--- StockThought.py
+++ StockThought.py
@@ -1,5 +1,17 @@
 ## @author Valeri Kozarev
 import sys
+import tweepy
+import Tkinter
+
+##keys
+consumer_key = 'consumer key'
+consumer_secret = 'consumer secrets'
+access_token = 'access token'
+access_token_secret = 'access token secret'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
 
 TwitterURL = ""
 RedditURl = ""

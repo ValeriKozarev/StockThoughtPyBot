4b9865a98e5a5b180e8fa07082ac7f349b8fdf0c
vikozare <vikozare@ncsu.edu>
2018-11-03 21:02:49 -0400

    First commit

---
+++ StockThought.py
@@ -0,0 +1,22 @@
+## @author Valeri Kozarev
+import sys
+
+TwitterURL = ""
+RedditURl = ""
+
+def main():
+    """Main entry point for the script."""
+    pass
+
+def getTopPost(url):
+    "Return a link to the top post of reddit at the current hour"
+
+def getTopComment(url):
+    "Return a string representing the top comment from the top post"
+    pass
+
+def makeTweet(url):
+    "post that top comment to the twitter account"
+    
+if __name__ == '__main__':
+    sys.exit(main())
